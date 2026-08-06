8afa918cfa9044e3b57129990655d810550f4021
Danielkombou <danielkombou7@gmail.com>
2026-08-06 09:03:52 +0100

    adding crosslines

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start relative z-10">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start relative overflow-hidden">
+        <div className="cross-lines" />
         <Image
           className="dark:invert h-5 w-[100px]"
           src="/next.svg"
