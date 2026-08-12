f6e9500b5820259b70268b2760e06eee5d486928
sha-nou <njingtishanelle103@gmail.com>
2026-08-12 13:03:29 +0100

    adding content

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start relative overflow-hidden">
+    <div className="flex flex-col flex-1 items-center justify-center bg-background font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 border border-border rounded-lg m-8 sm:items-start">
         <div className="relative z-10 flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="text-3xl font-semibold leading-10 tracking-tight text-foreground">
             Daniel Kombou
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+          <p className="max-w-md text-lg leading-8 text-zinc-400">
             Full Stack Developer
           </p>
         </div>
